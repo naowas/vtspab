f24421a5d1eb235e0f1dd6b1d47dcb6320f61648
Naowas Morshed Eimon <naowas.morshed@gmail.com>
2025-01-02 11:19:39 +0600

    member

--- resources/views/filament/member/custom/brand.blade.php
+++ resources/views/filament/member/custom/brand.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col">
         <span class="text-lg font-bold">{{Config::get('app.name')}}</span>
     </div>
-    @if(auth()->check() && auth()->user()->company->logo)
+    @if(auth()->check() && auth()->user()?->company?->logo)
         <img src="{{ Storage::url(auth()->user()->company->logo) }}" alt="Logo" class="h-10"/>
     @endif
 </div>
