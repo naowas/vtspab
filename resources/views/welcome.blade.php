f9063ea6e84de8e394ec89dd04c500d176f990cf
Naowas Morshed Eimon <naowas.morshed@gmail.com>
2025-01-06 12:03:00 +0600

    member

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -333,22 +333,23 @@
         <!-- Section Title -->
         <div class="container section-title" data-aos="fade-up">
             <h2>24 Licensed Companies Together as One.</h2>
-            <p>Leading the telemtics industry of Bangladesh for last 15 years.</p>
+            <p>Leading the telematics industry of Bangladesh for the last 15 years.</p>
         </div>
 
         <div class="container">
             <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
                 <div class="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
                     @foreach($companies as $company)
-                        <div class="col-lg-3 col-md-6 portfolio-item isotope-item filter-app">
+                        <div class="col-lg-3 col-md-6 col-6 portfolio-item isotope-item filter-app">
                             <div class="portfolio-content h-100">
                                 <div class="image-wrapper">
                                     <img src="{{Storage::url($company->logo)}}" class="img-fluid" alt="{{$company->name}}">
                                 </div>
                                 <div class="portfolio-info">
                                     <p>{{$company->name}}</p>
-                                    <a target="_blank" href="{{$company->website}}" title="More Details"
-                                       class="details-link"><i class="bi bi-link-45deg"></i></a>
+                                    <a target="_blank" href="{{$company->website}}" title="More Details" class="details-link">
+                                        <i class="bi bi-link-45deg"></i>
+                                    </a>
                                 </div>
                             </div>
                         </div>
