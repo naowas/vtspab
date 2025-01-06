5ec78ec6620e46c65daf3a3c47a1efa21e511481
Naowas Morshed Eimon <naowas.morshed@gmail.com>
2025-01-06 12:55:01 +0600

    member

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -5,7 +5,7 @@
     <section id="team" class="team section">
 
         <!-- Section Title -->
-        <div class="container section-title aos-init aos-animate text-left" data-aos="fade-up" style="margin-top: 100px" >
+        <div class="container section-title aos-init aos-animate text-left" data-aos="fade-up" style="margin-top: 100px">
             <p>{{$type}}</p>
         </div><!-- End Section Title -->
 
@@ -14,26 +14,21 @@
             <div class="row gy-4">
 
                 @foreach($members as $m)
-                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-3 col-md-6 col-6 d-flex align-items-stretch aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
                         <div class="team-member">
                             <div class="member-img">
                                 @if($m->getFirstMediaUrl('founding-member-images') !== '')
-                                <img src="{{$m->getFirstMediaUrl('founding-member-images')}}" class="img-fluid" alt="">
+                                    <img src="{{$m->getFirstMediaUrl('founding-member-images')}}" class="img-fluid" alt="">
                                 @else
-                                <img src="{{asset('assets/img/dft.png')}}" class="img-fluid" alt="">
+                                    <img src="{{asset('assets/img/dft.png')}}" class="img-fluid" alt="">
                                 @endif
                                 <div class="social">
-{{--                                    <a href="{{$m->twitter}}"><i class="bi bi-twitter-x"></i></a>--}}
-{{--                                    <a href="{{$m->facebook}}"><i class="bi bi-facebook"></i></a>--}}
-{{--                                    <a href="{{$m->instagram}}"><i class="bi bi-instagram"></i></a>--}}
                                     <a href="{{$m->linkedin}}"><i class="bi bi-linkedin"></i></a>
                                 </div>
                             </div>
                             <div class="member-info">
                                 <h4>{{$m->name}}</h4>
-{{--                                <span>{{$m->designation}}, </span> <span>{{$m->company}}</span>--}}
                                 <span>{{$m->designation}} </span>
-{{--                                <p></p>--}}
                             </div>
                         </div>
                     </div><!-- End Team Member -->
@@ -45,3 +40,14 @@
 </main>
 
 @include('layouts.footer')
+
+<style>
+    @media (max-width: 768px) {
+        .team .team-member .member-info h4 {
+            font-size: 15px;
+        }
+        .team .team-member .member-info span {
+            font-size: 12px;
+        }
+    }
+</style>
